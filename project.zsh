2cded7860a42765413c8a50c6d665da0d093c568
Luka Vandervelden <lukc@upyum.com>
2015-01-04 18:50:02 +0100

    Deprecated comment removed.

--- project.zsh
+++ project.zsh
@@ -11,10 +11,6 @@ dist=(
 	build.zsh build/*.zsh project.zsh
 )
 
-# Notes:
-#   - We’re using -D_BSD_SOURCE because we don’t want to rewrite common stuff
-#     that’s present on most OSes such games could be played on (GNU/*, *BSD)
-
 CC=clang
 LDFLAGS=""
 CFLAGS="-Wall -Wextra -pedantic -g"
